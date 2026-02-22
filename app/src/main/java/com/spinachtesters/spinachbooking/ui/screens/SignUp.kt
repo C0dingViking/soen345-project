7edbd22624f2bdde55c44665bd6ae1c1766e4151
C0dingViking <william.ross.lpp.83@gmail.com>
2026-02-22 10:27:34 -0500

    made content scrollable since `Have an account? Login` section was not visible on my device

--- app/src/main/java/com/spinachtesters/spinachbooking/ui/screens/SignUp.kt
+++ app/src/main/java/com/spinachtesters/spinachbooking/ui/screens/SignUp.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.Icon
 //noinspection UsingMaterialAndMaterial3Libraries
@@ -50,7 +52,8 @@ fun SignUpScreen() {
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(innerPadding)
-                    .padding(16.dp),
+                    .padding(16.dp)
+                    .verticalScroll(rememberScrollState()),
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
